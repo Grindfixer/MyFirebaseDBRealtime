45408a7f54d2a04fa5bd64ebaefff701a96f6145
jwn812 <john.nicholson812@gmail.com>
2019-06-02 09:51:55 -0400

    First commit-vid @ 24:11

---
+++ app/src/main/java/android/jwn/myfirebasedbrealtime/MyRecyclerViewHolder.java
@@ -0,0 +1,17 @@
+package android.jwn.myfirebasedbrealtime;
+
+import android.support.annotation.NonNull;
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MyRecyclerViewHolder extends RecyclerView.ViewHolder {
+    TextView txt_title,txt_comment;
+    public MyRecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txt_comment = (TextView)itemView.findViewById(R.id.txt_content);
+        txt_title = (TextView)itemView.findViewById(R.id.txt_title);
+    }
+}
